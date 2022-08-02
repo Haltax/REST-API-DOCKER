02cec9ee9c98f2bd81e071e60b4723ea34db72c2
Haltax <97481101+Haltax@users.noreply.github.com>
2022-08-02 16:15:23 +0200

    Add files via upload

--- pierwszy.py
+++ pierwszy.py
@@ -1,9 +1,16 @@
 from flask import Flask, request
 import pika
+import argparse
 
+parser = argparse.ArgumentParser()
 
-rabbitmq_host = input("Enter the host ip: ")
-rabbitmq_queue = input("Enter the queue: ")
+parser.add_argument("-i", "--rabbit_ip", default="127.0.0.1", help="Rabbitmq IP address")
+parser.add_argument("-q", "--queue", default="new", help="Rabbitmq queue name")
+args = vars(parser.parse_args())
+
+rabbitmq_host = args["rabbit_ip"]
+rabbitmq_queue = args["queue"]
+
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host=rabbitmq_host))
@@ -13,21 +20,23 @@ channel.queue_declare(queue=rabbitmq_queue)
 
 app = Flask(__name__)
 
-
 @app.route('/add', methods=["POST"])
+
 def process_json():
     content_type = request.headers.get('Content-Type')
     if(content_type == 'application/json'):
-
-        json = request.json
-        tmp = str(json)
-        channel.basic_publish(
-            exchange='', routing_key=rabbitmq_queue, body=tmp)
-
+    
+    
+        json=request.json
+        tmp=str(json)
+        channel.basic_publish(exchange='', routing_key=rabbitmq_queue, body=tmp)
+        
+        
         return json
-
+         
+            
     else:
-        return '400 Bad Request'
+        return '400 Bad Request'  
 
 
 if __name__ == '__main__':

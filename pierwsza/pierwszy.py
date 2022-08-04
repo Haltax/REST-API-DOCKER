77c62d2c39b1726567e6d91accd976a38a985917
Haltax <97481101+Haltax@users.noreply.github.com>
2022-08-04 13:54:58 +0200

    Update pierwszy.py

--- pierwsza/pierwszy.py
+++ pierwsza/pierwszy.py
@@ -1,16 +1,17 @@
 from flask import Flask, request
 import pika
-import argparse
+from configparser import ConfigParser
 
-parser = argparse.ArgumentParser()
+#Read config.ini file
+config_object = ConfigParser()
+config_object.read("config.ini")
 
-parser.add_argument("-i", "--rabbit_ip", default="127.0.0.1", help="Rabbitmq IP address")
-parser.add_argument("-q", "--queue", default="new", help="Rabbitmq queue name")
-args = vars(parser.parse_args())
+#Get the rabbit ip and queue
+rabitinfo = config_object["Rabbit"]
+rabbitmq_host=format(rabitinfo["Rabbit_IP"])
+rabbitmq_queue=format(rabitinfo["Rabbit_queue"])
 
-rabbitmq_host = args["rabbit_ip"]
-rabbitmq_queue = args["queue"]
-
+#connect to rabbitmq server
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host=rabbitmq_host))
@@ -22,22 +23,25 @@ app = Flask(__name__)
 
 @app.route('/add', methods=["POST"])
 
+#function that checks if input is json type
+
 def process_json():
     content_type = request.headers.get('Content-Type')
     if(content_type == 'application/json'):
-    
-    
+
+
         json=request.json
         tmp=str(json)
         channel.basic_publish(exchange='', routing_key=rabbitmq_queue, body=tmp)
-        
-        
+
+
         return json
-         
-            
+
+
     else:
-        return '400 Bad Request'  
+        return '400 Bad Request'
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0',port=5000)
+
